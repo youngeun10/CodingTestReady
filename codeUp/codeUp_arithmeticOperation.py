ff27b7765ba00ef83da8d4f4d7a15c1a1238ee43
youngeun_office <youwoonon@naver.com>
2022-05-18 08:40:58 +0900

    20220518_기초(산술연산)

--- codeUp/codeUp_arithmeticOperation.py
+++ codeUp/codeUp_arithmeticOperation.py
@@ -3,6 +3,16 @@ n = int(input())
 print(-n)
 """
 
-""" 33. 문자 1개 입력받아 다음 문자 출력하기 """
+""" 33. 문자 1개 입력받아 다음 문자 출력하기 
 a = ord(input()) + 1
 print(chr(a))
+"""
+
+""" 34. 정수 2개 입력받아 차 계산하기 
+a, b = map(int, input().split())
+print(a-b)
+"""
+
+""" 35. 실수 2개 입력받아 곱 계산 """
+a, b = map(float, input().split())
+print(a*b)
