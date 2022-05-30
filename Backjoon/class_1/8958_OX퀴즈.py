d23b47ff899ee218a7ca5093258a40410b52db96
김영은 <yevelop10@gmail.com>
2022-05-30 16:53:08 +0900

    220530_class 1+

--- Backjoon/class_1/8958_OX퀴즈.py
+++ Backjoon/class_1/8958_OX퀴즈.py
@@ -1,14 +1,29 @@
 n = int(input())
 result = []
-for i in range(0, 5):
+for i in range(0, n):
     arr = list(input())
-    tmp = list(range(len(arr)))
+    tmp = []
     for j in arr:
-        print(j)
-        # if arr[j] == 'X':
-        #     tmp.append(0)
-        # elif arr[j] == 'O':
-        #     tmp.append(1) if j == 0 else tmp.append(arr[j-1]+1)
+        if j == 'X':
+            tmp.append(0)
+        elif j == 'O':
+            tmp.append(1) if len(tmp) == 0 else tmp.append(tmp[-1]+1)
     result.append(sum(tmp))
-print(result)
+for i in result: print(i)
 
+# memory : 30840    time : 76
+"""
+import sys
+N = int(sys.stdin.readline())
+for i in range(N):
+    quiz_result = sys.stdin.readline()
+    accum = 1
+    score = 0
+    for q in quiz_result:
+        if q is 'O':
+            score += accum
+            accum += 1
+        else:
+            accum = 1
+    print(score)
+"""
