f0030e4f2a0820d40ec80a8855422ccbe08f3a95
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-07-02 18:07:18 +0900

    220702_점점 어려워 지나봐~

---
+++ Backjoon/step/12_집합과 맵/220702_10815_숫자카드.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline())
+sg = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+cmp = list(map(int, sys.stdin.readline().split()))
+
+cmp.sort()
+
+
